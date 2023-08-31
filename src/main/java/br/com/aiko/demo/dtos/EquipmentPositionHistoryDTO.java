f88e3401256dbb4b36f7fc3005fd6c94e3e9588b
Davisoad <davibragaz18@gmail.com>
2023-08-31 11:28:42 -0300

    Project Aiko

---
+++ src/main/java/br/com/aiko/demo/dtos/EquipmentPositionHistoryDTO.java
@@ -0,0 +1,2 @@
+package br.com.aiko.demo.dtos;public record EquipmentPositionHistoryDTO() {
+}
---
+++ src/main/java/br/com/aiko/demo/services/EquipmentModelStateHourlyEarningsService.java
@@ -0,0 +1,2 @@
+package br.com.aiko.demo.services;public class EquipmentModelStateHourlyEarningsService {
+}
